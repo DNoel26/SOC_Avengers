a6170fa9ed3640e38ec3761c7cc6e6f993ca0f4c
Dnoel26 <darnellnoel.webdev@gmail.com>
2020-05-08 23:40:14 +0100

    second commit

--- js/BusinessLogic/dirSelector.js
+++ js/BusinessLogic/dirSelector.js
@@ -6,20 +6,10 @@ const Direction_Selector = {
     forward_direction : true,
 
     user_direction_sel()
-    {
-        if(this.left_arrow == false && this.right_arrow == false)
-        {
-            this.forward_direction = true;
-        }
-        
-        else if(this.left_arrow == true && this.right_arrow == false)
+    { 
+        if(this.left_arrow == true && this.right_arrow == false)
         {
             this.forward_direction = false;
-        }
-
-        else if(this.right_arrow == true && this.left_arrow == false)
-        {
-            this.forward_direction = true;
         }
 
         return this.forward_direction;
